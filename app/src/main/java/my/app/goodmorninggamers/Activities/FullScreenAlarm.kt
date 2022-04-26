d939e4dab413b4c3904a18323e0e538a66582d9c
Hamish <hamish.develop@gmail.com>
2022-04-26 17:47:55 +1200

    26/04/2022
    -Wait had to enable javascript shit is actually working
    
    WHY IS IT MUTED THOUGH>?>?>?>

--- app/src/main/java/my/app/goodmorninggamers/Activities/FullScreenAlarm.kt
+++ app/src/main/java/my/app/goodmorninggamers/Activities/FullScreenAlarm.kt
@@ -1,20 +1,24 @@
 package my.app.goodmorninggamers.Activities
 
 import android.os.Bundle
-import android.view.View
 import android.webkit.WebView
 import androidx.appcompat.app.AppCompatActivity
 import my.app.goodmorninggamers.Activities.SetAlarmScreen_Components.turnScreenOffAndKeyguardOn
 import my.app.goodmorninggamers.Activities.SetAlarmScreen_Components.turnScreenOnAndKeyguardOff
 import my.app.goodmorninggamers.R
 
+
 class FullScreenAlarm : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_full_screen_alarm)
         turnScreenOnAndKeyguardOff()
-        var mWebView = findViewById<View>(R.id.webview) as WebView
-        mWebView.loadUrl("https://main--coruscating-heliotrope-63f939.netlify.app")
+        //var mWebView = findViewById<View>(R.id.webview) as WebView
+        val myWebView = WebView(this)
+        val webSettings = myWebView.settings
+        webSettings.javaScriptEnabled = true
+        setContentView(myWebView)
+        myWebView.loadUrl("https://main--coruscating-heliotrope-63f939.netlify.app")
 
 
     }
